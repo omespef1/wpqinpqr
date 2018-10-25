fae483d88daade4914814601e7edf45854a5280e
omespef1 <omespef1@hotmail.com>
2018-10-24 19:26:51 -0500

    v

--- src/app/components/ec/eccotiz/eccotiz.component.ts
+++ src/app/components/ec/eccotiz/eccotiz.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eccotiz.component.css']
 })
 export class EccotizComponent implements OnInit {
-
+   par_busq: any= {
+     ter_coda:string,
+     ter_nomb:string,
+     fec_fini:Date,
+     fec_fina:Date
+   }
   constructor() { }
 
   ngOnInit() {
